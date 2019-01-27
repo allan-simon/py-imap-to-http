ecb833aa6d6276e2d9bfd713112b7437ae5524db
Allan Simon <allan.simon@supinfo.com>
2019-01-27 12:33:23 +0100

    corrected typo

--- service/__main__.py
+++ service/__main__.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(
         idle_loop(
-            IMAP_SERVER
+            IMAP_SERVER,
             IMAP_USER,
             IMAP_PASSWORD,
         )
